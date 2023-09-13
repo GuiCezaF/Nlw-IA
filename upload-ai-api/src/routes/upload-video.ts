562ef10c922e02564cd475bd447117278fa85e94
GuiCezaF <contato.guicezafe@gmail.com>
2023-09-13 13:06:08 -0300

    feat: create upload video route

---
+++ upload-ai-api/src/routes/upload-video.ts
@@ -0,0 +1,41 @@
+import { FastifyInstance } from "fastify";
+import { fastifyMultipart } from "@fastify/multipart";
+import path from "node:path";
+import { randomUUID } from "node:crypto";
+import { pipeline } from "node:stream";
+import fs from "node:fs";
+import { promisify } from "node:util";
+
+const pump = promisify(pipeline);
+
+export async function uploadVideoRoute(app: FastifyInstance) {
+  app.register(fastifyMultipart, {
+    limits: {
+      fileSize: 1_048_576 * 25, //25mb
+    },
+  });
+  app.post("/videos", async (req, reply) => {
+    const data = await req.file();
+
+    if (!data) {
+      return reply.status(400).send("Missing file input");
+    }
+
+    const extensions = path.extname(data.filename);
+
+    if (extensions != ".mp3") {
+      return reply.status(400).send("Invalid input type, please upload a MP3.");
+    }
+
+    const fileBaseName = path.basename(data.filename, extensions);
+    const fileUploadName = `${fileBaseName}-${randomUUID()}${extensions}`;
+
+    const uploadDestination = path.resolve(
+      __dirname,
+      "../../temp",
+      fileUploadName,
+    );
+
+    await pump(data.file, fs.createWriteStream(uploadDestination));
+  });
+}
--- upload-ai-api/src/server.ts
+++ upload-ai-api/src/server.ts
@@ -1,14 +1,18 @@
-import {fastify} from 'fastify'
-import { getAllPromptsRoute } from './routes/get-all-prompts';
+import { fastify } from "fastify";
+import { getAllPromptsRoute } from "./routes/get-all-prompts";
+import { uploadVideoRoute } from "./routes/upload-video";
 
-const app = fastify()
+const app = fastify();
 
 const port = 3333;
 
-app.register(getAllPromptsRoute)
+app.register(getAllPromptsRoute);
+app.register(uploadVideoRoute);
 
-app.listen({
-  port: port,
-}).then(() => {
-  console.log(`HTTP Server Running at port :${port} `)
-})
+app
+  .listen({
+    port: port,
+  })
+  .then(() => {
+    console.log(`HTTP Server Running at port :${port} `);
+  });
